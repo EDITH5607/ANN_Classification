9fdbeb5d9496666b9b626e6c8c64ef6dac7d7414
EDITH5607 <aadharshajith5607@gmail.com>
2025-08-16 23:50:23 +0530

    app.py modified

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
 import streamlit as st
 import tensorflow as tf
 
